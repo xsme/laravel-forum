c58aba7cf1e58c85919090f4a52757c4ce72bf82
xsme <sebastian.rzeszutek@itmotion.pl>
2020-11-03 09:25:13 +0100

    Uzupelnienie migracji forums

--- database/migrations/2020_11_03_081935_create_forums_table.php
+++ database/migrations/2020_11_03_081935_create_forums_table.php
@@ -15,7 +15,20 @@ class CreateForumsTable extends Migration
     {
         Schema::create('forums', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('parent_id')->nullable()->default(null);
+            $table->unsignedBigInteger('left_id')->nullable()->default(null);
+            $table->unsignedBigInteger('right_id')->nullable()->default(null);
+            $table->string('name');
+            $table->text('description')->nullable()->default(null);
+            $table->tinyInteger('type')->default(0);
+            $table->tinyInteger('status')->default(0);
+            $table->string('link')->nullable()->default(null);
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('parent_id')->references('id')->on('forums');
+            $table->foreign('left_id')->references('id')->on('forums');
+            $table->foreign('right_id')->references('id')->on('forums');
         });
     }
 
