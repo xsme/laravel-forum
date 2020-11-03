f759239f6500f9575844606a305e9dbb1d23a938
xsme <sebastian.rzeszutek@itmotion.pl>
2020-11-03 10:26:48 +0100

    Dodanie seeder dla topic

--- database/seeders/TopicSeeder.php
+++ database/seeders/TopicSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Topic;
 use Illuminate\Database\Seeder;
 
 class TopicSeeder extends Seeder
@@ -13,6 +14,6 @@ class TopicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Topic::factory()->times(100)->create();
     }
 }
